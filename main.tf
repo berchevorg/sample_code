1d42611bbdd65d97cd870049d1a8d8d1f1140acd
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2021-09-09 10:36:57 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,76 @@
+resource "random_pet" "name1" {
+ length    = "2"
+ separator = "-"
+}
+
+resource "random_pet" "name2" {
+ length    = "2"
+ separator = "-"
+}
+
+resource "random_pet" "name3" {
+ length    = "2"
+ separator = "-"
+}
+
+resource "random_pet" "name4" {
+ length    = "2"
+ separator = "-"
+}
+
+resource "random_pet" "name5" {
+ length    = "2"
+ separator = "-"
+}
+
+resource "random_pet" "name6" {
+ length    = "2"
+ separator = "-"
+} 
+
+resource "random_pet" "name7" {
+ length    = "2"
+ separator = "-"
+}
+
+resource "null_resource" "randompet1" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name1.id}"
+  }
+}
+
+resource "null_resource" "randompet2" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name2.id}"
+  }
+}
+
+resource "null_resource" "randompet3" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name3.id}"
+  }
+}
+
+resource "null_resource" "randompet4" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name4.id}"
+  }
+}
+
+resource "null_resource" "randompet5" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name5.id}"
+  }
+}
+
+resource "null_resource" "randompet6" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name6.id}"
+  }
+}
+
+resource "null_resource" "randompet7" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.name7.id}"
+  }
+}
